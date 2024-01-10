3f1e14c055fc17ff9796533e410a0ba0606952b7
Alinochka07 <alina011988@icloud.com>
2024-01-10 16:32:45 +0600

    task4-4.1

---
+++ dynamodb-project/fillDataToDynamoDBTable.ts
@@ -0,0 +1,41 @@
+import * as AWS from "aws-sdk";
+import { DynamoDBDocument, PutCommandInput } from "@aws-sdk/lib-dynamodb";
+import { DynamoDB } from "@aws-sdk/client-dynamodb";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+// JS SDK v3 does not support global configuration.
+// Codemod has attempted to pass values to each service client in this file.
+// You may need to update clients outside of this file, if they use global config.
+AWS.config.update({
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    region: process.env.AWS_REGION,
+});
+
+const dynamoDB = DynamoDBDocument.from(new DynamoDB());
+
+const tableName = "Products_DynamoDB";
+
+const item = {
+    id: "2fksdkf-vwsfjsAWfd",
+    title: "Item inserted using script",
+    price: 30,
+    description: "This is the description of Item that inserted using scripts, for Task-4"
+}
+
+const params: PutCommandInput = {
+    TableName: tableName,
+    Item: item
+}
+
+dynamoDB.put(params, (err, data) => {
+    if (err) {
+        console.error("Error inserting item", err)
+    } else {
+        console.log("Item inserted sucessfulyy", data)
+    }
+});
+
+
